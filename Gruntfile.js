1951a4d1823e9eafc668a5fd06e0d92e5edf3bbc
Nandam, Nagachaitanya <nagachaitanya.nandam@oracle.com>
2022-01-29 16:59:59 +0530

    end of course1

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    const sass = require('node-sass');
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                },
+                options: {
+                    implementation: sass,
+                    outputStyle: 'expanded',
+                    sourceMap: true,
+                    quiet: true // stop depreciation errors
+                }
+            }
+            
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
