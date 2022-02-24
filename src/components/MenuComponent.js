9f07629d62cfdb91557223b92825364c228342d1
Nandam, Nagachaitanya <nagachaitanya.nandam@oracle.com>
2022-02-24 22:47:16 +0530

    Single Page Applications

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
-            <Card
-                onClick={() => onClick(dish.id)}>
+            <Card>
+                <Link to={`/menu/${dish.id}`}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
+                    <CardTitle><p class="font-weight-bold">{dish.name}</p></CardTitle>
                 </CardImgOverlay>
+                </Link>
             </Card>
         );
     }
@@ -25,11 +27,25 @@ import { Card, CardImg, CardImgOverlay,
         });
 
         return (
+            <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+            </div>
             <div className="container">
                 <div className="row">
                     {menu}
                 </div>
             </div>
+            </React.Fragment>
         );
     }
 
