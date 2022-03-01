f16703a58666b305cdcc084e60df769885f1b29b
Nandam, Nagachaitanya <nagachaitanya.nandam@oracle.com>
2022-03-01 22:00:55 +0530

    React Assignment2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -44,12 +45,14 @@ class Main extends Component {
          </React.Fragment>
       );
     }
+
     return (
       <Switch>
         <Route path='/home' component={HomePage} />
         <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>} />
         <Route path='/menu/:dishId' component={DishWithId} />
         <Route exact path='/contactus' component={Contact} /> 
+        <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>}  />
         <Redirect to='/home' />
       </Switch>
     );
