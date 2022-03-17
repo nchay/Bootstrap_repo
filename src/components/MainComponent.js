6c7366006cdb01a69c2690f669895cbbfa595784
Nandam, Nagachaitanya <nagachaitanya.nandam@oracle.com>
2022-03-17 16:48:39 +0530

    Fetch from Server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments,fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
@@ -23,13 +23,17 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   addComment:(dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
+  fetchComments: () => { dispatch(fetchComments())},
+  fetchPromos: () => { dispatch(fetchPromos())},
 })
 
 
 class Main extends Component {
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
   render() {
     const DishWithId = ({match}) => {
@@ -37,7 +41,8 @@ class Main extends Component {
           <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
             isLoading={this.props.dishes.isLoading}
             errMess={this.props.dishes.errMess}
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
+            comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            commentsErrMess={this.props.comments.errMess} 
             addComment={this.props.addComment}/>
       );
     };
@@ -50,7 +55,9 @@ class Main extends Component {
                 dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
                 dishesLoading={this.props.dishes.isLoading}
                 dishesErrMess={this.props.dishes.errMess}
-                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                promoLoading={this.props.promotions.isLoading}
+              promoErrMess={this.props.promotions.errMess}
                 leader={this.props.leaders.filter((leader) => leader.featured)[0]}
             />
           <Footer /> 
@@ -61,7 +68,7 @@ class Main extends Component {
     return (
       <Switch>
         <Route path='/home' component={HomePage} />
-        <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>} />
+        <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes.dishes}/>} />
         <Route path='/menu/:dishId' component={DishWithId} />
         <Route exact path='/contactus' component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm} />} /> 
         <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>}  />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,12 +3,13 @@ import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
                 <Link to={`/menu/${dish.id}`}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle><p class="font-weight-bold">{dish.name}</p></CardTitle>
                 </CardImgOverlay>
@@ -19,7 +20,7 @@ import { Loading } from './LoadingComponent';
 
     const Menu = (props) => {
 
-        const menu = props.dishes.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
                     <RenderMenuItem dish={dish} onClick={props.onClick} />
